834dbce10c1be24ef6ff64d30b1696e17be348ca
mateusz <mat646@gmail.com>
2018-01-07 11:29:04 +0100

    Modified create_account. Added create_lecturer and book_conference_day.

--- functions/src/current_conference_price.sql
+++ functions/src/current_conference_price.sql
@@ -1,15 +1,15 @@
-CREATE FUNCTION current_conference_price (@ConferenceID INT, @ApplyStudentDiscount BIT = FALSE )
+CREATE FUNCTION current_conference_price (@ConferenceID INTEGER, @ApplyStudentDiscount BIT)
   RETURNS MONEY
 AS
   BEGIN
-    DECLARE @CurrentPrice INT, @StudentDiscount INT;
+    DECLARE @CurrentPrice INTEGER, @StudentDiscount INTEGER;
 
-    SELECT TOP 1 @CurrentPrice = ConferencePriceThresholds.Price, @StudentDiscount = ConferencePriceThresholds.StudentDiscount
+    SELECT TOP 1 @CurrentPrice = ConferencePriceThresholds.Price, @StudentDiscount = ConferencePriceThresholds.Discount
     FROM ConferencePriceThresholds
     WHERE ConferencePriceThresholds.ConferenceID = @ConferenceID AND ConferencePriceThresholds.StartDate < GETDATE()
     ORDER BY ConferencePriceThresholds.StartDate
 
-    IF (@ApplyStudentDiscount)
+    IF (@ApplyStudentDiscount = 1)
       BEGIN
         SET @CurrentPrice = @CurrentPrice * (1 - @StudentDiscount)
       END
--- procedures/src/create_account_paricipant.sql
+++ procedures/src/create_account_paricipant.sql
@@ -1,11 +1,11 @@
 CREATE PROCEDURE dbo.create_account_participant
-    @ClientName VARCHAR(255), @FirstName VARCHAR(255), @LastName VARCHAR(255), @Email VARCHAR(255), @Company VARCHAR(255),
+    @ClientName VARCHAR(255), @FirstName VARCHAR(255), @LastName VARCHAR(255), @Email VARCHAR(255), 
     @StudentID  VARCHAR(255)
 AS
   BEGIN
     INSERT INTO Clients (ClientName, Email, IsCompany) VALUES (@ClientName, @Email, 0)
-    INSERT INTO Participants (ClientID, FirstName, LastName, Email, Company, StudentID)
+    INSERT INTO Participants (ClientID, FirstName, LastName, Email, StudentID)
     VALUES ((SELECT SCOPE_IDENTITY()), @FirstName, @LastName,
-            @Email, @Company, @StudentID)
+            @Email, @StudentID)
   END
 
