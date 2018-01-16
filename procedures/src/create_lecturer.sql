417fcc3a10b503d1000d7dbb84527b1c9f342616
pzal <piotr.2445@gmail.com>
2018-01-16 18:56:57 +0100

    cdwa

--- procedures/src/create_lecturer.sql
+++
@@ -1,7 +0,0 @@
-CREATE PROCEDURE dbo.create_lecturer
-	@FirstName VARCHAR(255), @LastName VARCHAR(255)
-AS
-  BEGIN
-	INSERT INTO Lecturers (FirstName, LastName) VALUES (@FirstName, @LastName)
-  END
-
--- procedures/src/create_participant_for_client.sql
+++ procedures/src/create_participant_for_client.sql
@@ -1,6 +1,6 @@
-CREATE PROCEDURE dbo.register_participant
+CREATE PROCEDURE dbo.create_participant_for_client
     @ClientID VARCHAR(255), @FirstName VARCHAR(255), @LastName VARCHAR(255), @Email VARCHAR(255), 
-    @StudentID  VARCHAR(255)
+    @StudentID  VARCHAR(255) = NULL
 AS
   BEGIN
     INSERT INTO Participants (ClientID, FirstName, LastName, Email, StudentID)
