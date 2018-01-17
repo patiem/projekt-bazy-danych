4dee2bc27c92e688573f568a14554cf8a95075f7
pzal <piotr.2445@gmail.com>
2018-01-17 09:47:40 +0100

    cwdawda

--- roles.sql
+++ roles.sql
@@ -1,8 +1,29 @@
-CREATE ROLE Owner
 CREATE ROLE Employee
 
-GRANT ALL
-TO Owner
+DENY DELETE, INSERT, UPDATE
+  ON Clients
+  TO Employee
+DENY DELETE, INSERT, UPDATE
+  ON Participants
+  TO Employee
+DENY DELETE, INSERT, UPDATE
+  ON Conferences
+  TO Employee
+DENY DELETE, INSERT, UPDATE
+  ON Workshops
+  TO Employee
+DENY DELETE, INSERT, UPDATE
+  ON RegistrationDateRanges
+  TO Employee
+DENY DELETE, INSERT, UPDATE
+  ON RegistrationsForConferences
+  TO Employee
+DENY DELETE, INSERT, UPDATE
+  ON RegistrationsForWorkshops
+  TO Employee
+DENY DELETE, INSERT, UPDATE
+  ON ConferencePriceThresholds
+  TO Employee
 
 GRANT SELECT ON
   dbo.ConferenceRegistrationsToCancelView
