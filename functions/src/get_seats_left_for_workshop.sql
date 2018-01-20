895a029cf482d8fa1330e07108c83005b2455288
mateusz <mat646@gmail.com>
2018-01-20 21:55:47 +0100

    Added LecturerID in procedure 'create_workshop'.

--- functions/src/get_seats_left_for_workshop.sql
+++ functions/src/get_seats_left_for_workshop.sql
@@ -2,13 +2,13 @@ CREATE FUNCTION get_seats_left_for_workshop (@WorkshopID INT)
   RETURNS INT
 AS
   BEGIN
-    SET @NumberOfRegistrations = (
+    DECLARE @NumberOfRegistrations INT = (
       SELECT Count(*) FROM RegistrationsForWorkshops WHERE RegistrationsForWorkshops.WorkshopID = @WorkshopID
     )
-    SET @NumberOfSeatsForWorkshop = (
+    DECLARE @NumberOfSeatsForWorkshop INT = (
       SELECT Workshops.NumberOfSeats FROM Workshops WHERE Workshops.WorkshopID = @WorkshopID
     )
-    SET @SeatsLeft = @NumberOfSeatsForWorkshop - @NumberOfRegistrations
+    DECLARE @SeatsLeft INT = @NumberOfSeatsForWorkshop - @NumberOfRegistrations
 
     RETURN @SeatsLeft
   END
--- procedures/src/create_workshop_for_conference.sql
+++ procedures/src/create_workshop_for_conference.sql
@@ -1,6 +1,6 @@
 CREATE PROCEDURE dbo.create_workshop_for_conference
-  @WorkshopName VARCHAR(255), @ConferenceID INTEGER, @NumberOFSeats INTEGER, @StartDateTime DATE, @EndDateTime DATE, @Price FLOAT
+  @WorkshopName VARCHAR(255), @ConferenceID INTEGER, @NumberOFSeats INTEGER, @StartDateTime DATE, @EndDateTime DATE, @Price FLOAT, @LecturerID INT
 AS
   BEGIN
-    INSERT INTO Workshops VALUES (@WorkshopName, @ConferenceID, @NumberOfSeats, @StartDateTime, @EndDateTime, @Price)
+    INSERT INTO Workshops VALUES (@WorkshopName, @ConferenceID, @NumberOfSeats, @StartDateTime, @EndDateTime, @Price, @LecturerID)
   END 
