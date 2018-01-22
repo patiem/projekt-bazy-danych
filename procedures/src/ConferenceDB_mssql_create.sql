9572f8fa5b61d252c466f08dee96a15b1ef69f89
pzal <piotr.2445@gmail.com>
2018-01-22 23:20:26 +0100

    drop if exists (2)

--- procedures/src/ConferenceDB_mssql_create.sql
+++ procedures/src/ConferenceDB_mssql_create.sql
@@ -1,3 +1,15 @@
+IF OBJECT_ID('Workshops_fk0') IS NOT NULL ALTER TABLE Workshops DROP CONSTRAINT Workshops_fk0
+IF OBJECT_ID('Workshops_fk1') IS NOT NULL ALTER TABLE Workshops DROP CONSTRAINT Workshops_fk1
+IF OBJECT_ID('Participants_fk0') IS NOT NULL ALTER TABLE Participants DROP CONSTRAINT Participants_fk0
+IF OBJECT_ID('RegistrationDateRanges_fk0') IS NOT NULL ALTER TABLE RegistrationDateRanges DROP CONSTRAINT RegistrationDateRanges_fk0
+IF OBJECT_ID('RegistrationsForConferences_fk1') IS NOT NULL ALTER TABLE RegistrationsForConferences DROP CONSTRAINT RegistrationsForConferences_fk1
+IF OBJECT_ID('RegistrationsForConferences_fk0') IS NOT NULL ALTER TABLE RegistrationsForConferences DROP CONSTRAINT RegistrationsForConferences_fk1
+IF OBJECT_ID('RegistrationsForWorkshops_fk0') IS NOT NULL ALTER TABLE RegistrationsForWorkshops DROP CONSTRAINT RegistrationsForWorkshops_fk0
+IF OBJECT_ID('RegistrationsForWorkshops_fk1') IS NOT NULL ALTER TABLE RegistrationsForWorkshops DROP CONSTRAINT RegistrationsForWorkshops_fk1
+IF OBJECT_ID('ConferencePriceThresholds_fk0') IS NOT NULL ALTER TABLE ConferencePriceThresholds DROP CONSTRAINT ConferencePriceThresholds_fk0
+GO
+
+IF OBJECT_ID('Workshops') IS NOT NULL DROP TABLE Workshops
 CREATE TABLE [Workshops] (
 	WorkshopID integer NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	WorkshopName varchar(255) NOT NULL,
@@ -9,6 +21,7 @@ CREATE TABLE [Workshops] (
 	LecturerID integer NOT NULL,
 )
 GO
+IF OBJECT_ID('Clients') IS NOT NULL DROP TABLE Clients
 CREATE TABLE [Clients] (
 	ClientID integer NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	ClientName varchar(255) NOT NULL,
@@ -16,6 +29,7 @@ CREATE TABLE [Clients] (
 	IsCompany binary NOT NULL,
 )
 GO
+IF OBJECT_ID('Participants') IS NOT NULL DROP TABLE Participants
 CREATE TABLE [Participants] (
 	ParticipantID integer NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	ClientID integer NOT NULL,
@@ -25,6 +39,7 @@ CREATE TABLE [Participants] (
 	StudentID varchar(255),
 )
 GO
+IF OBJECT_ID('Conferences') IS NOT NULL DROP TABLE Conferences
 CREATE TABLE [Conferences] (
 	ConferenceID integer NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	ConferenceName varchar(255) NOT NULL,
@@ -33,6 +48,7 @@ CREATE TABLE [Conferences] (
 	EndDate date NOT NULL,
 )
 GO
+IF OBJECT_ID('RegistrationDateRanges') IS NOT NULL DROP TABLE RegistrationDateRanges
 CREATE TABLE [RegistrationDateRanges] (
 	RegistrationDateRangeID integer NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	RegistrationForConferenceID integer NOT NULL,
@@ -40,6 +56,7 @@ CREATE TABLE [RegistrationDateRanges] (
 	EndDate date NOT NULL,
 )
 GO
+IF OBJECT_ID('RegistrationsForConferences') IS NOT NULL DROP TABLE RegistrationsForConferences
 CREATE TABLE [RegistrationsForConferences] (
 	RegistrationForConferenceID integer NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	ConferenceID integer NOT NULL,
@@ -47,6 +64,7 @@ CREATE TABLE [RegistrationsForConferences] (
 	PaidAt date,
 )
 GO
+IF OBJECT_ID('RegistrationsForWorkshops') IS NOT NULL DROP TABLE RegistrationsForWorkshops
 CREATE TABLE [RegistrationsForWorkshops] (
 	RegistrationForWorkshopID integer NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	WorkshopID integer NOT NULL,
@@ -54,12 +72,14 @@ CREATE TABLE [RegistrationsForWorkshops] (
 	PaidAt date,
 )
 GO
+IF OBJECT_ID('Lecturers') IS NOT NULL DROP TABLE Lecturers
 CREATE TABLE [Lecturers] (
 	LecturerID integer NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	FirstName varchar(255) NOT NULL,
 	LastName varchar(255) NOT NULL,
 )
 GO
+IF OBJECT_ID('ConferencePriceThresholds') IS NOT NULL DROP TABLE ConferencePriceThresholds
 CREATE TABLE [ConferencePriceThresholds] (
 	ConferencePriceThresholdID integer NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	ConferenceID integer NOT NULL,
@@ -68,48 +88,41 @@ CREATE TABLE [ConferencePriceThresholds] (
 	Discount float NOT NULL,
 )
 GO
+
+IF OBJECT_ID('Workshops_fk0') IS NOT NULL ALTER TABLE Workshops DROP CONSTRAINT Workshops_fk0
 ALTER TABLE [Workshops] WITH CHECK ADD CONSTRAINT [Workshops_fk0] FOREIGN KEY ([ConferenceID]) REFERENCES [Conferences]([ConferenceID])
 GO
-ALTER TABLE [Workshops] CHECK CONSTRAINT [Workshops_fk0]
-GO
+
+IF OBJECT_ID('Workshops_fk1') IS NOT NULL ALTER TABLE Workshops DROP CONSTRAINT Workshops_fk1
 ALTER TABLE [Workshops] WITH CHECK ADD CONSTRAINT [Workshops_fk1] FOREIGN KEY ([LecturerID]) REFERENCES [Lecturers]([LecturerID])
 GO
-ALTER TABLE [Workshops] CHECK CONSTRAINT [Workshops_fk1]
-GO
 
 
+IF OBJECT_ID('Participants_fk0') IS NOT NULL ALTER TABLE Participants DROP CONSTRAINT Participants_fk0
 ALTER TABLE [Participants] WITH CHECK ADD CONSTRAINT [Participants_fk0] FOREIGN KEY ([ClientID]) REFERENCES [Clients]([ClientID])
 GO
-ALTER TABLE [Participants] CHECK CONSTRAINT [Participants_fk0]
-GO
 
 
+IF OBJECT_ID('RegistrationDateRanges_fk0') IS NOT NULL ALTER TABLE RegistrationDateRanges DROP CONSTRAINT RegistrationDateRanges_fk0
 ALTER TABLE [RegistrationDateRanges] WITH CHECK ADD CONSTRAINT [RegistrationDateRanges_fk0] FOREIGN KEY ([RegistrationForConferenceID]) REFERENCES [RegistrationsForConferences]([RegistrationForConferenceID])
 GO
-ALTER TABLE [RegistrationDateRanges] CHECK CONSTRAINT [RegistrationDateRanges_fk0]
-GO
 
+IF OBJECT_ID('RegistrationsForConferences_fk0') IS NOT NULL ALTER TABLE RegistrationsForConferences DROP CONSTRAINT RegistrationsForConferences_fk0
 ALTER TABLE [RegistrationsForConferences] WITH CHECK ADD CONSTRAINT [RegistrationsForConferences_fk0] FOREIGN KEY ([ConferenceID]) REFERENCES [Conferences]([ConferenceID])
 GO
-ALTER TABLE [RegistrationsForConferences] CHECK CONSTRAINT [RegistrationsForConferences_fk0]
-GO
+IF OBJECT_ID('RegistrationsForConferences_fk1') IS NOT NULL ALTER TABLE RegistrationsForConferences DROP CONSTRAINT RegistrationsForConferences_fk1
 ALTER TABLE [RegistrationsForConferences] WITH CHECK ADD CONSTRAINT [RegistrationsForConferences_fk1] FOREIGN KEY ([ParticipantID]) REFERENCES [Participants]([ParticipantID])
 GO
-ALTER TABLE [RegistrationsForConferences] CHECK CONSTRAINT [RegistrationsForConferences_fk1]
-GO
 
+IF OBJECT_ID('RegistrationsForWorkshops_fk0') IS NOT NULL ALTER TABLE RegistrationsForWorkshops DROP CONSTRAINT RegistrationsForWorkshops_fk0
 ALTER TABLE [RegistrationsForWorkshops] WITH CHECK ADD CONSTRAINT [RegistrationsForWorkshops_fk0] FOREIGN KEY ([WorkshopID]) REFERENCES [Workshops]([WorkshopID])
 GO
-ALTER TABLE [RegistrationsForWorkshops] CHECK CONSTRAINT [RegistrationsForWorkshops_fk0]
-GO
+IF OBJECT_ID('RegistrationsForWorkshops_fk1') IS NOT NULL ALTER TABLE RegistrationsForWorkshops DROP CONSTRAINT RegistrationsForWorkshops_fk1
 ALTER TABLE [RegistrationsForWorkshops] WITH CHECK ADD CONSTRAINT [RegistrationsForWorkshops_fk1] FOREIGN KEY ([ParticipantID]) REFERENCES [Participants]([ParticipantID])
 GO
-ALTER TABLE [RegistrationsForWorkshops] CHECK CONSTRAINT [RegistrationsForWorkshops_fk1]
-GO
 
 
+IF OBJECT_ID('ConferencePriceThresholds_fk0') IS NOT NULL ALTER TABLE ConferencePriceThresholds DROP CONSTRAINT ConferencePriceThresholds_fk0
 ALTER TABLE [ConferencePriceThresholds] WITH CHECK ADD CONSTRAINT [ConferencePriceThresholds_fk0] FOREIGN KEY ([ConferenceID]) REFERENCES [Conferences]([ConferenceID])
-GO
-ALTER TABLE [ConferencePriceThresholds] CHECK CONSTRAINT [ConferencePriceThresholds_fk0]
 GO
 
