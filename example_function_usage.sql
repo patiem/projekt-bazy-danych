e88b965bd20ad920fa298dcbd88582a12950f6a1
mateusz <mat646@gmail.com>
2018-01-22 21:23:51 +0100

    Modified example usage.

--- example_function_usage.sql
+++ example_function_usage.sql
@@ -1,5 +1,5 @@
 DECLARE @val1 INT;
-SET @val1 = dbo.current_conference_price(3,1);
+SET @val1 = dbo.get_current_conference_price(3,1);
 SELECT @val1
 
 DECLARE @val1 INT;
@@ -7,7 +7,7 @@ SET @val1 = dbo.get_seats_left_for_workshop(5)
 SELECT @val1
 
 DECLARE @val1 INT;
-SET @val1 = dbo.get_seats_left_for_conference_at_date(1, '2017-08-16')
+SET @val1 = dbo.get_seats_left_for_conference_at_date(1, '2017-11-17')
 SELECT @val1
 
 SELECT * FROM report_best_clients(1)
