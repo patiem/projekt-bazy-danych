941c0c0231a818ddd76693a6177523c081e894cd
pzal <piotr.2445@gmail.com>
2018-01-23 09:24:40 +0100

    more fixes

--- procedures/src/Procedure_function_trigger_creator.sql
+++ procedures/src/Procedure_function_trigger_creator.sql
@@ -417,14 +417,15 @@ CREATE FUNCTION dbo.report_payments (@ConferenceID INT)
 AS
   RETURN(
   SELECT
-    FirstName, LastName, 'Conference' as Type, PaidAt FROM RegistrationsForConferences
+    FirstName, LastName, 'Conference' as Type, PaidAt
+  FROM RegistrationsForConferences
     INNER JOIN Participants ON Participants.ParticipantID = RegistrationsForConferences.ParticipantID
   WHERE ConferenceID = @ConferenceID
 
 UNION
 
   SELECT
-    FirstName,LastName, CONVERT(varchar(255), Workshops.WorkshopID), PaidAt
+    FirstName, LastName, 'Workshop ' + CONVERT(varchar(255), Workshops.WorkshopID), PaidAt
   FROM RegistrationsForWorkshops
     INNER JOIN Participants ON Participants.ParticipantID = RegistrationsForWorkshops.ParticipantID
     INNER JOIN Workshops ON Workshops.WorkshopID = RegistrationsForWorkshops.WorkshopID
